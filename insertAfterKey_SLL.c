1958a7778b265b4d0c6095a4338a6f3e44387d4d
aa5649 <101615849+aa5649@users.noreply.github.com>
2023-07-20 08:10:39 +0530

    Create insertAfterKey_SLL.c
    implemented insertion after a key on a single linked list in C

---
+++ insertAfterKey_SLL.c
@@ -0,0 +1,62 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void createNode();
+void insertAtMiddleAfterKey(int val,int key,int n);
+void display();
+
+struct node{
+    int data;
+    struct node *next;
+};
+
+struct node *head = NULL;
+
+int main(){
+    int n,i;
+    scanf("%d",&n);
+    head = malloc(sizeof(struct node));
+    scanf("%d",&head->data);
+    for(i=1;i<n;i++)createNode();
+    int val,key;
+    scanf("%d %d",&val,&key);
+    insertAtMiddleAfterKey(val,key,n);
+    display();
+    return 0;
+}
+
+void createNode(){
+    struct node *temp,*ptr;
+    temp = malloc(sizeof(struct node));
+    scanf("%d",&temp->data);
+    ptr=head;
+    while(ptr->next!=NULL){
+        ptr=ptr->next;}
+    ptr->next = temp;
+    temp->next = NULL;
+}
+
+void insertAtMiddleAfterKey(int val,int key,int n){
+    struct node *ptr = head->next,*p=head,*temp;
+    temp = malloc(sizeof(struct node));
+    temp->data=val;
+    while(ptr->data!=key){
+        ptr=ptr->next;
+        p=p->next;
+        count++;
+    }
+        ptr=ptr->next;
+        p=p->next;
+    p->next = temp;
+    temp->next = ptr;
+    
+    
+}
+
+
+void display(){
+    struct node *ptr = head;
+    while(ptr!=NULL){
+        printf("%d\t",ptr->data);
+        ptr = ptr->next;
+    }
