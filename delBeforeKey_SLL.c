6b96805be1815dfd3a4184c0f086adc073518648
aa5649 <101615849+aa5649@users.noreply.github.com>
2023-07-26 23:59:36 +0530

    Create delBeforeKey_SLL.c
    implemented deletion before key on a SLL in C

---
+++ delBeforeKey_SLL.c
@@ -0,0 +1,61 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void createNode();
+void delBeforeKey(int key);
+void display();
+
+struct node *head = NULL;
+
+struct node{
+    int data;
+    struct node *next;
+};
+
+int main(){
+    int n,i;
+    printf("Enter the value of n: ");
+    scanf("%d",&n);
+    head = malloc(sizeof(struct node));
+    scanf("%d",&head->data);
+    for(i=1;i<n;i++){
+        printf("Enter the value of node %d: ",i+1);
+        createNode();
+    }
+    int key;
+    printf("Enter the value whose previous node is to be deleted: ");
+    scanf("%d",&key);
+    delBeforeKey(key);
+    display();
+    return 0;
+}
+
+void createNode(){
+    struct node *temp,*ptr;
+    temp = malloc(sizeof(struct node));
+    scanf("%d",&temp->data);
+    ptr=head;
+    while(ptr->next!=NULL){
+        ptr=ptr->next;}
+    ptr->next = temp;
+}
+
+void delBeforeKey(int key){
+    struct node *ptr,*p;
+    ptr=head->next;
+    p=head;
+    while(ptr->data!=key){
+        ptr=ptr->next;
+        p=p->next;
+    }
+    p->next = ptr->next;
+    free(ptr);
+}
+
+void display(){
+    struct node *ptr = head;
+    while(ptr!=NULL){
+        printf("%d\t",ptr->data);
+        ptr=ptr->next;
+    }
+}
